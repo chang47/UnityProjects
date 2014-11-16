984bc2cb425a771eab7ec09f2dcadbee25a41745
chang47 <coralbue@gmail.com>
2014-11-15 16:01:20 -0800

    moving camera

---
+++ ZonbieConga/Assets/BackgroundController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+	private Transform cameraTransform;
+	private float spriteWidth;
+	// Use this for initialization
+	void Start () {
+		cameraTransform = Camera.main.transform;
+		SpriteRenderer spriteRenderer = renderer as SpriteRenderer;
+		spriteWidth = spriteRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
