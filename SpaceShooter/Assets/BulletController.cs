30f8c436010626720a58ecd87eb5dbb1069eab41
chang47 <coralbue@gmail.com>
2014-11-29 00:29:56 -0800

    space ship moving

---
+++ SpaceShooter/Assets/BulletController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = new Vector2(0, speed);
+	}
+	
+	// Update is called once per frame
+	void OnBecameInvisible () {
+		Destroy (gameObject);
+	}
+}
---
+++ SpaceShooter/Assets/SpaceShipController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShipController : MonoBehaviour {
+	
+	public Vector2 speed = new Vector2 (5, 5);
+	public GameObject bullet;
+	private Vector2 movement;
+
+	public float fireRate = 0.5f;
+	private float nextFire = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY
+		);
+		if(Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (bullet, transform.position, Quaternion.identity);
+		}
+	}
+	
+	void FixedUpdate() {
+		rigidbody2D.velocity = movement;
+	}
+}
