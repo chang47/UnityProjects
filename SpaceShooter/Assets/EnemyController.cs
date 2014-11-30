91bdc7de568f1507af5f8e18999bf45fad55e201
chang47 <coralbue@gmail.com>
2014-11-30 01:14:21 -0800

    moving enemy

---
+++ SpaceShooter/Assets/EnemyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	public float speed = -1f;
+
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2(0, speed);	
+		rigidbody2D.angularVelocity = Random.Range(-200, 200);
+
+	}
+}
